2fbc3cde2934e6f3f04efdc01024d93949e5df2d
ruffiano <ruffianodev@gmail.com>
2023-01-29 21:52:37 +0900

    man_test --fixed

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -14,12 +14,12 @@ var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	config, err := util.LoadConfig(".")
+	config, err := util.LoadConfig("../..")
 	if err != nil {
-		log.Fatal("cannot load config: ", err)
+		log.Fatal("cannot load config:", err)
 	}
 
-	testDB, err := sql.Open(config.DBDriver, config.DBSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
